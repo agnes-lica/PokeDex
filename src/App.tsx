8f27791dc1487e6b8d2e100fe62edc5f35a4a717
agnesLica <agnes.lica@gmail.com>
2023-08-25 20:13:34 -0400

    feat: making structure to consume the API

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import RoutesApplication from './routes';
 
-function App() {
+export function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+      
+        <RoutesApplication/>
+  
     </div>
   );
 }
